086f59a971cbd94768298c84f3c4396f16c07872
raweber <raweber@student.42wolfsburg.de>
2023-03-04 13:32:32 +0100

    added percentiles

--- describe.py
+++ describe.py
@@ -14,6 +14,7 @@ def main():
 		df = pd.read_csv(sys.argv[1])
 	except:
 		print(f"{colors().RED}Error: could not read file{colors().END}")
+		print(sys.argv[1])
 		exit()
 
 
@@ -42,36 +43,96 @@ def main():
 	first_percentile = 0
 	second_percentile = 0
 	third_percentile = 0
-	mean = 0
-	std = 0
+	mean = 0.0
+	std = 0.0
 	for column in df:
-		count = 0
-		mean = 0
+		count = 0.0
+		mean = 0.0
 		min = float(df[column][0])
 		max = 0
 		std = 0
+		first_percentile = 0
+		second_percentile = 0
+		third_percentile = 0
+		
+		# sort column ascending for percentiles
+		df.sort_values(by=[column], inplace=True)
+			
+
 		for value in df[column]:
-			if value != "":
-				count += 1 # this counts too many!
+			if math.isnan(value) == False:
+				count += 1 # this counts also empty values!
+			else:
+				value = 0
+			# if count == n * 0.25:
+			# 	first_percentile = value
+			# elif count == n * 0.5:
+			# 	second_percentile = value
+			# elif count == n * 0.75:
+			# 	third_percentile = value
 			if float(value) < min:
 				min = float(value)
 			if float(value) > max:
 				max = float(value)
-			mean += float(value)
-			print(f"MEAN inner: {mean}, value: {value}")
-		mean /= count
-
+			mean = mean + float(value)
+		
+		# print(f"MEAN inner: {mean}, value: {float(value)}, count: {count}")
+		mean = mean / count
 
 		# second round for std and the percentiles
 		for value in df[column]:
+			if math.isnan(value) == True:
+				value = 0
 			std += (float(value) - mean) ** 2
 		
 		std /= (count - 1)
 		std = math.sqrt(std)
-		print(f"count: {count}, min: {min}, max: {max}, mean: {mean}, std: {std}")
+	
+		percentile_count = 0
+		for value in df[column]:
+			if math.isnan(value) == False:
+				percentile_count += 1
+			else:
+				value = 0
+			
+			# WARNING: check again, what happens if percentile is EXACTLY the index
+			
+			# linear interpolation: i + (j - i) * fraction, where fraction is the fractional part of the index surrounded by i and j.
+			if percentile_count == round(count * 0.25):
+				first_percentile = value
+			print((count * 0.25), (count * 0.25).is_integer())
+			if not ((count - 1) * 0.25).is_integer():
+				if percentile_count == round(count * 0.25) + 1:
+					first_percentile = first_percentile + ((value - first_percentile) * (((count - 1) * 0.25) - math.floor((count - 1) * 0.25)))
+
+			if percentile_count == round(count * 0.5):
+				second_percentile = value
+			if not ((count - 1 * 0.5)).is_integer():
+				if percentile_count == round(count * 0.5) + 1:
+					second_percentile = second_percentile + ((value - second_percentile) * (((count - 1) * 0.5) - math.floor((count - 1) * 0.5)))
+
+			if percentile_count == round(count * 0.75):
+				third_percentile = value
+			if not ((count - 1) * 0.75).is_integer():
+				if percentile_count == round(count * 0.75) + 1:
+					third_percentile = third_percentile + ((value - third_percentile) * (((count - 1) * 0.75) - math.floor((count - 1) * 0.75)))
+					break #nothing more to do
+
+	print(f"first percentile: {first_percentile:.6f}, second percentile: {second_percentile:.6f}, third_percentile: {third_percentile:.6f}")
+	print(f"count: {count:.6f}, min: {min:.6f}, max: {max:.6f}, mean: {mean:.6f}, std: {std:.6f}")
+
+		# check std of first Feature again!
 		
 			
+ 
 
+# Step 1: Arrange all data values in the data set in ascending order.
+# Step 2: Count the number of values in the data set where it is represented as 'n'.
+# Step 3: calculate the value of k/100, where k = any number between zero and one hundred.
+# Step 4: Multiply 'k' percent by 'n'.The resultant number is called an index.
+# Step 5: If the resultant index is not a whole number then round to the nearest whole number, then go to Step 7. If the index is a whole number, then go to Step 6. 
+# Step 6: Count the values in your data set from left to right until you reach the number. Then find the mean for that corresponding number and the next number. The resultant value is the kth percentile of your data set. 
+# Step 7: Count the values in your data set from left to right until you reach the number. The obtained value will be the kth percentile of your data set.
 
 
 			
